c6a4f359050cefff5245b8e73e1d3ef44868e147
AlexK411 <33633119+AlexK411@users.noreply.github.com>
2017-11-13 11:47:54 -0500

    first upload

---
+++ personality_survey.py
@@ -0,0 +1,38 @@
+print ("what's your favorute sport?")
+sport = input()
+
+if sport == "hockey":
+    print ("that's my favort too!")
+
+elif sport == "curling":
+    print("cool!")
+elif sport == "ping pong":
+    print ("great!")
+
+
+else:
+    print(sport + " sounds fun!")
+
+
+print ("whats your favorite tv show?")
+show = input()
+if show == "the office":
+    print ("who is your favorite charachter?")
+    character = input()
+    if character == "Dwight":
+        print ("beets, bears, battlestar galactica")
+    elif character == "jim":
+        print ("let's put a stapler in the jello!")
+
+elif show == "The Flash":
+    print ("That show is my favorite show too!")
+else:
+    print
+    (show + " Sounds good!")
+
+print ("what's your favorite video game?")
+videogame = input()
+if videogame == "Black Ops 2":
+    print ("That's my favorite too!")
+else:
+    print ("that's a great game!")
